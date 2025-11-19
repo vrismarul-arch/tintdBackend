96148d2ef2eb380f6c33529c4932ede82fe1ee70
vrismarul-arch <vrismarul@gmail.com>
2025-11-20 01:37:51 +0530

    zoho mail

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,16 +1,14 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.in",     // 🇮🇳 India Zoho Server (best for your domain vrism.in)
-  port: 465,                // SSL Port
-  secure: true,             // SSL must be true on 465
-  auth: {
-    user: process.env.MAIL_USER,  // your Zoho email (full email)
-    pass: process.env.MAIL_PASS,  // your Zoho App Password
-  },
+  host: process.env.MAIL_HOST,
+port: process.env.MAIL_PORT,
+secure: true,
+auth: {
+  user: process.env.MAIL_USER,
+  pass: process.env.MAIL_PASS,
+}
+
 });
 
 export default transporter;
